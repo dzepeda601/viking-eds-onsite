13c45440ca3a77b009012c6e2f4ee17fbe386eb9
timwlee <timothyl@adobe.com>
2025-03-09 16:06:22 -0700

    Update paths.json

--- blocks/video-banner/video-banner.js
+++ blocks/video-banner/video-banner.js
@@ -3,10 +3,7 @@ import { makeVideo } from '../../scripts/scripts.js';
 export default async function decorate(block) {  
   const videoSrc = block.querySelector('div a');
 
-  const aem = 'https://author-p142507-e1463170.adobeaemcloud.com';
-  const url = `${aem}${videoSrc.text}`;
-
-  videoSrc.href = url;
+  videoSrc.href = videoSrc.text;
   makeVideo(block.querySelector('div'), videoSrc.href);
   videoSrc.remove();
 }
