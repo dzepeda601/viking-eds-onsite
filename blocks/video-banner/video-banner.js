8ddcdf6625e268887a8db932ea81dd28cf2e8ce4
timwlee <timothyl@adobe.com>
2025-03-09 16:19:24 -0700

    Test commit for video banner

--- blocks/video-banner/video-banner.js
+++ blocks/video-banner/video-banner.js
@@ -2,8 +2,7 @@ import { makeVideo } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {  
   const videoSrc = block.querySelector('div a');
-
-  videoSrc.href = videoSrc.text;
+  videoSrc.href = videoSrc.href;
   makeVideo(block.querySelector('div'), videoSrc.href);
   videoSrc.remove();
 }
