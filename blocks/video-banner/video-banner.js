09405b90ee013d71e861487708475a04753f3cd0
timwlee <timothyl@adobe.com>
2025-03-09 16:00:35 -0700

    test commit

--- blocks/video-banner/video-banner.js
+++ blocks/video-banner/video-banner.js
@@ -3,7 +3,10 @@ import { makeVideo } from '../../scripts/scripts.js';
 export default async function decorate(block) {  
   const videoSrc = block.querySelector('div a');
 
-  videoSrc.href = videoSrc.text;
+  const aem = 'https://author-p142507-e1463170.adobeaemcloud.com';
+  const url = `${aem}${videoSrc.text}`;
+
+  videoSrc.href = url;
   makeVideo(block.querySelector('div'), videoSrc.href);
   videoSrc.remove();
 }
