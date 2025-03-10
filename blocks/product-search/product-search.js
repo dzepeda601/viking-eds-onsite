b51eeb3dcd5d6e20b8ddfe5365721be8b893bf53
timwlee <timothyl@adobe.com>
2025-03-10 09:34:47 -0700

    Add product-search block

--- blocks/product-search/product-search.js
+++ blocks/product-search/product-search.js
@@ -1,3 +1,27 @@
 export default function decorate(block) {
   console.log("product search block: ", block);
+  block.querySelector(':scope > div:first-of-type').classList.add('title');
+
+  const div = document.createElement('div');
+  div.classList.add('search-container')
+  div.innerHTML = `
+    <select>
+      <option value="" disabled selected>CRUISE TYPE</option>
+      <option value="Rivers">Rivers</option>
+      <option value="Oceans">Oceans</option>
+      <option value="Expeditions">Expeditions</option>
+    </select>
+    <select disabled>
+      <option value="" disabled selected>REGION</option>
+    </select>
+    <select disabled>
+      <option value="" disabled selected>DEPARTURE DATE</option>
+    </select>
+    <button type="button" class="search-button">
+      <img src="/icons/search.svg" width="14px" height="14px" alt="search icon">
+      <span>SEARCH</span>
+    </button>
+  `;
+
+  block.append(div);
 }
