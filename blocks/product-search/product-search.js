2fa8700e54ec469608c3b9aa1cd7c0b9b72f14b8
timwlee <timothyl@adobe.com>
2025-03-10 12:51:59 -0700

    Add labels to select input and hide it

--- blocks/product-search/product-search.js
+++ blocks/product-search/product-search.js
@@ -2,20 +2,26 @@ export default function decorate(block) {
   block.querySelector(':scope > div:first-of-type').classList.add('title');
 
   const div = document.createElement('div');
-  div.classList.add('search-container')
+  div.classList.add('search-container');
   div.innerHTML = `
-    <select>
+    <label for="cruise-type">CRUISE TYPE</label>
+    <select id="cruise-type">
       <option value="" disabled selected>CRUISE TYPE</option>
       <option value="Rivers">Rivers</option>
       <option value="Oceans">Oceans</option>
       <option value="Expeditions">Expeditions</option>
     </select>
-    <select disabled>
+    
+    <label for="region">REGION</label>
+    <select id="region" disabled>
       <option value="" disabled selected>REGION</option>
     </select>
-    <select disabled>
+    
+    <label for="departure-date">DEPARTURE DATE</label>
+    <select id="departure-date" disabled>
       <option value="" disabled selected>DEPARTURE DATE</option>
     </select>
+    
     <button type="button" class="search-button">
       <img src="/icons/search.svg" width="14px" height="14px" alt="search icon">
       <span>SEARCH</span>
