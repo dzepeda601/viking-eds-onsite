7b15935935175b8524119327cf1f435d8fb4fdce
timwlee <timothyl@adobe.com>
2025-03-09 17:24:33 -0700

    Update video-banner block

--- blocks/video-banner/video-banner.js
+++ blocks/video-banner/video-banner.js
@@ -5,4 +5,8 @@ export default async function decorate(block) {
   videoSrc.href = videoSrc.href;
   makeVideo(block.querySelector('div'), videoSrc.href);
   videoSrc.remove();
+
+
+  block.querySelector(':scope > div:nth-of-type(1)').classList.add('video-container');
+  block.querySelector(':scope > div:nth-of-type(2)').classList.add('logo-container');
 }
